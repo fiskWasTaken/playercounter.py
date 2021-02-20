88a06a2bbba4e750a043629ece3524318f515930
fisk <fiskie@live.co.uk>
2021-02-20 10:13:50 +0000

    init

---
+++ README.md
@@ -0,0 +1,3 @@
+# playercounter.py
+
+playercounter.com is a scam website that generates fake player counts with a random number generator.
---
+++ playercounter.py
@@ -0,0 +1,14 @@
+from bs4 import BeautifulSoup
+import requests
+import sys
+
+def get_count(game) -> int:
+    r=requests.get('https://playercounter.com/%s/' % game)
+    soup = BeautifulSoup(r.content, features="html.parser")
+    return int(soup.find(class_='entry-content').h2.get_text().split()[0].replace(',',''))
+
+try:
+    print(get_count(sys.argv[1])
+except IndexError:
+    print("supply a game name (i.e. overwatch)")
+    exit(1)
