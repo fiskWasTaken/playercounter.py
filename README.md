3ab7f01c291aab4a35b34acc8b701578ac5881cb
fisk <fiskie@live.co.uk>
2021-02-20 10:17:43 +0000

    update readme

--- README.md
+++ README.md
@@ -1,3 +1,19 @@
 # playercounter.py
 
 playercounter.com is a scam website that generates fake player counts with a random number generator.
+
+`./playercounter.py overwatch`
+
+```
+% for i in {1..10}; do ./playercounter.py overwatch; done
+812805
+802180
+791125
+692974
+780883
+806864
+721663
+721475
+800600
+731444
+```
--- playercounter.py
+++ playercounter.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 from bs4 import BeautifulSoup
 import requests
 import sys
@@ -8,7 +9,7 @@ def get_count(game) -> int:
     return int(soup.find(class_='entry-content').h2.get_text().split()[0].replace(',',''))
 
 try:
-    print(get_count(sys.argv[1])
+    print(get_count(sys.argv[1]))
 except IndexError:
     print("supply a game name (i.e. overwatch)")
     exit(1)
